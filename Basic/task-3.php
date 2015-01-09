86295529a7d37eaa6a0a981395b4ed1ad336d5e9
abukowsar <m.a.kowsar@gmail.com>
2015-01-09 08:47:40 +0600

    HTML

---
+++ Basic/task-3.php
@@ -0,0 +1,10 @@
+
+<?php
+$identity='James Bond';
+$car='BMW';
+$sentence="$identity drives a $car";
+
+
+echo "$sentence";
+echo "<br>";
+
---
+++ Basic/task-5.php
@@ -0,0 +1,14 @@
+
+<?php
+$actor_agent='Agent';
+$actor_neo='Neo';
+echo "$actor_agent : Who do you think you are, any how?";
+echo "<br>";
+echo "$actor_neo : I am $actor_neo, but my people call me the One";
+
+echo "<br>";
+echo "<br>";
+
+echo "$actor_agent : Who do you think you are, any how?";
+echo "<br>";
+echo "$actor_neo : I am $actor_neo, but my people call me the One";
